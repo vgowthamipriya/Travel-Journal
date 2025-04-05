9f722365be1c827cd056ae3178a6dc94c84eae69
vgowthamipriya <vinnakotagowthamipriya9@gmail.com>
2025-04-05 21:54:01 +0530

    final commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="https://webstockreview.net/images/globe-vector-png-14.png" />
+            <h1><b>My Travel Journal</b></h1>
+        </header>
+    )
+}
---
+++ src/data.js
@@ -0,0 +1,74 @@
+export default [
+    {
+        id: 1,
+        img: {
+            src: "https://images.pexels.com/photos/1530259/pexels-photo-1530259.jpeg?cs=srgb&dl=architecture-building-city-1530259.jpg&fm=jpg",
+            alt: "Eiffel Tower"
+        },
+        title: "    ",
+        country: "France",
+        googleMapsLink: "https://www.google.com/maps?q=Eiffel+Tower,+Paris,+France",
+        dates: "14 Feb, 2023 - 20 Feb, 2023",
+        text: "The Eiffel Tower is one of the most recognizable landmarks in the world, located in the heart of Paris. It attracts millions of tourists every year."
+    },
+    {
+        id: 2,
+        img: {
+            src: "https://th.bing.com/th/id/OIP.urO_8eNQoqiPHSBoD0EGOAHaFj?rs=1&pid=ImgDetMain",
+            alt: "Mount Fuji"
+        },
+        title: "Mount Fuji",
+        country: "Japan",
+        googleMapsLink: "https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
+        dates: "12 Jan, 2022 - 24 Jan, 2022",
+        text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters. It is Japan’s most iconic natural landmark and a popular hiking destination."
+    },
+    {
+        id: 3,
+        img: {
+            src: "https://okapi-travel.com/wp-content/uploads/2019/02/grand-canyon-1170x919.jpg",
+            alt: "Grand Canyon"
+        },
+        title: "Grand Canyon",
+        country: "USA",
+        googleMapsLink: "https://www.google.com/maps?q=Grand+Canyon",
+        dates: "01 Apr, 2021 - 15 Apr, 2021",
+        text: "The Grand Canyon is a breathtaking natural wonder in Arizona. Its immense size and stunning views make it one of the most visited national parks in the USA."
+    },
+    {
+        id: 4,
+        img: {
+            src: "https://wallpaperaccess.com/full/107786.jpg",
+            alt: "Taj Mahal"
+        },
+        title: "Taj Mahal",
+        country: "India",
+        googleMapsLink: "https://www.google.com/maps?q=Taj+Mahal,+Agra,+India",
+        dates: "01 Apr, 2020 - 15 Apr, 2020",
+        text: "The Taj Mahal in Agra is a UNESCO World Heritage site and a symbol of love. Built by Mughal emperor Shah Jahan, it is admired for its stunning white marble architecture."
+    },
+    {
+        id: 5,
+        img: {
+            src: "https://i1.wp.com/carriemaguirewriter.com/wp-content/uploads/2019/04/1D9E2C3C-1EE6-4A26-B5A1-D92BFBDFB838.jpeg?ssl=1",
+            alt: "Machu Picchu"
+        },
+        title: "Machu Picchu",
+        country: "Peru",
+        googleMapsLink: "https://www.google.com/maps?q=Machu+Picchu,+Peru",
+        dates: "01 Apr, 2019 - 15 Apr, 2019",
+        text: "Machu Picchu is a 15th-century Inca citadel set high in the Andes Mountains. It’s one of the most iconic archaeological sites in the world."
+    },
+    {
+        id: 6,
+        img: {
+            src: "https://www.tripsavvy.com/thmb/DiGvDLLGkVQNJ50wu1uemVEPaQ0=/2128x1409/filters:fill(auto,1)/GettyImages-578190533-5954de075f9b5815d95af801.jpg",
+            alt: "Blue Lagoon"
+        },
+        title: "Blue Lagoon",
+        country: "Iceland",
+        googleMapsLink: "https://www.google.com/maps?q=Blue+Lagoon,+Iceland",
+        dates: "20 Aug, 2018 - 28 Aug, 2018",
+        text: "The Blue Lagoon is a geothermal spa located in a lava field near Grindavík. Its warm waters and rich minerals are known for their healing properties."
+    }
+]
